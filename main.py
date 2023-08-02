c5398b40755e5aaf2432c25919798a01cb023932
Nicolas Deleu <n.deleu@hotmail.fr>
2023-08-02 04:21:14 +0200

    secret dsn sentry

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import sentry_sdk
 import app.models
+from configparser import ConfigParser
 from app.controllers.first_init_database.check_empty_db import check_administrator_existence
 from app.controllers.first_init_database.create_first_admin import create_first_administrator
 from app.controllers.cli_controller import cli
 
 
 if __name__ == '__main__':
+    config = ConfigParser()
+    config.read('config.ini')
+
+    sentry_dsn = config.get('sentry', 'dsn', raw=True)
+
     sentry_sdk.init(
-        dsn="https://93a5e8099f404fb2ab2dc4c47dfcdecf@o4505604593876992.ingest.sentry.io/4505604598202368",
+        dsn=sentry_dsn,
 
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
